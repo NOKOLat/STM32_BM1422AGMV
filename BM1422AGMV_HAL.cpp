83a45176fd0ab5c0c0633e550c88ad9c67c52190
aoi-256 <aoi256jp@Gmail.com>
2025-03-14 09:05:07 +0900

    fix: クラスの継承関係を変更(#1)

--- BM1422AGMV_HAL.cpp
+++ BM1422AGMV_HAL.cpp
@@ -6,21 +6,20 @@
  */
 
 #include <BM1422AGMV_HAL.h>
-#include <i2c.h>
 
-BM1422AGMV_HAL::BM1422AGMV_HAL(I2C_HandleTypeDef *use_i2cPin){
+BM1422AGMV_HAL::BM1422AGMV_HAL(I2C_HandleTypeDef* I2cPin){
 
 	//使用するピンの情報を受け取る
-	i2c_pin = use_i2cPin;
+	this->I2cPin = I2cPin;
 
 }
 
-void BM1422AGMV_HAL::write(BM1422AGMV_HAL::REGISTER REGISTER_ADDR, uint8_t* command, uint8_t len){
+void BM1422AGMV_HAL::Write(BM1422AGMV::REGISTER REGADDR, uint8_t *TxBuffer, uint8_t Len){
 
-	HAL_I2C_Mem_Write(i2c_pin, I2C_ADDR, uint8_t(REGISTER_ADDR), 1, command, len, 10);
+	HAL_I2C_Mem_Write(I2cPin, I2CADDR, (uint8_t)REGADDR, 1, TxBuffer, Len, 100);
 }
 
-void BM1422AGMV_HAL::read(BM1422AGMV_HAL::REGISTER REGISTER_ADDR, uint8_t* receive_buffer, uint8_t len){
+void BM1422AGMV_HAL::Read(BM1422AGMV::REGISTER REGADDR, uint8_t *RxBuffer, uint8_t Len){
 
-	HAL_I2C_Mem_Read(i2c_pin, I2C_ADDR, uint8_t(REGISTER_ADDR), 1, receive_buffer, len, 10);
+	HAL_I2C_Mem_Read(I2cPin, I2CADDR, (uint8_t)REGADDR, 1, RxBuffer, Len, 100);
 }
